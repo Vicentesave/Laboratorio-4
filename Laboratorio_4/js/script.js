81fb1def4835531a0928c379c6eb2856663ca92c
Vicentesve <vicente.40080@gmail.com>
2021-07-09 18:12:56 -0500

    commit 9 de julio

--- Laboratorio_4/js/script.js
+++ Laboratorio_4/js/script.js
@@ -21,10 +21,25 @@ function postTodo(e) {
         return;
     } 
 
-    var currentListHTML = list.innerHTML;
+    /* var currentListHTML = list.innerHTML;
     list.innerHTML = currentListHTML + `<input type="checkbox" name="todo" /> ${toDoText} <br>`
-    
+    document.getElementById('toDoText').value = ''; */
+
+    var div = document.createElement("div");
+    var checkbox = document.createElement("input");
+    var label = document.createElement("label");
+
+    checkbox.type = "checkbox";
+    checkbox.name = "todo";
+
+    label.textContent = toDoText;
+
+    div.appendChild(checkbox);
+    div.appendChild(label);
+    list.appendChild(div);
+
     document.getElementById('toDoText').value = '';
+
 
 }
 
@@ -44,9 +59,24 @@ function markTodo(e) {
     }
 }
 
-function deleteTodo(e) {
-    e.preventDefault();
+function deleteTodo() {
+    
 
-    var list = document.getElementById('toDoList');
-    list.innerHTML = '';
+    checkBoxes = document.getElementsByName('todo');
+
+    /* checkBoxes.forEach(element => {
+        if (element.checked) {
+            element.parentElement.remove();
+        }
+    }); */
+
+    for (let i = 0; i < checkBoxes.length; i++) {
+        if(checkBoxes[i].checked) {
+            checkBoxes[i].parentElement.remove();
+            i=-1;
+        } 
+    }
+
+   /*  var list = document.getElementById('toDoList');
+    list.innerHTML = ''; */
 }
